11958670c7535524a4d2ec6149d1ce7db6da0c9c
tanzim <syedtanzimwajih@gmail.com>
2026-03-11 00:52:42 +0530

    docs: add documentation comments to ControllerClass & methods

--- src/controller.js
+++ src/controller.js
@@ -2,27 +2,37 @@ import { ProjectGenerator } from "./project.js";
 import { TodoGenerator } from "./todo.js";
 import { dataManager } from "./data.js";
 
+
+/**
+ * Handles all Project & Todo operations
+ * Acts as a bridge between UI and Data
+ */
 export class ControllerClass {
 
     constructor() {
+        // Loads saved projects when the app starts
         this.dataArray = dataManager.savedDataChecker();
     }
 
+    // Return all projects
     getData() {
         return this.dataArray;
     }
 
+    // Creates a new project and saves it
     generateProject(title) {
         const project = new ProjectGenerator(title);
         this.dataArray.push(project);
         dataManager.saveData(this.dataArray);
     }
 
+    // Deletes a project
     removeProject(removeId) {
         this.dataArray = this.dataArray.filter(p => p.id !== removeId);
         dataManager.saveData(this.dataArray);
     }
 
+    // Creates a todo and adds it to the project
     addTodoToProject(projectId, title, description, date, priority) {
         this.dataArray = dataManager.savedProjectReconstructor();
 
